76bebccb19a6b8f0d61a3703d4f877bfd4230e38
CJBlain <146327891+CJBlain@users.noreply.github.com>
2025-01-12 18:50:56 +0000

    Added models for route table

--- app/models.py
+++ app/models.py
@@ -5,10 +5,27 @@ db = SQLAlchemy()
 class Trail(db.model):
     __tablename__ = 'TRAIL'
     
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(255), nullable=True)
-    difficulty = db.Column(db.String(50), nullable=True)
-    
+    trail_id = db.Column(db.Integer, primary_key=True)  
+    trail_name = db.Column(db.String(100), nullable=False)  
+    trail_summary = db.Column(db.String(200))  
+    trail_description = db.Column(db.String(500)) 
+    difficulty = db.Column(db.String(50))  
+    location = db.Column(db.String(100))   
+    length = db.Column(db.Float) 
+    elevation_gain = db.Column(db.Float)  
+
     def __repr__(self):
-        return f'<Trail {self.name}>'
+        return f"<Trail {self.trail_name}>"
+
+class Route(db.Model):
+    __tablename__ = 'route'
+
+    route_id = db.Column(db.Integer, primary_key=True)  
+    trail_id = db.Column(db.Integer, db.ForeignKey('trail.trail_id'), nullable=False)  # Foreign key to Trail
+    route_type = db.Column(db.String(50))  
+
+   
+    trail = db.relationship('Trail', backref=db.backref('routes', lazy=True))
+
+    def __repr__(self):
+        return f"<Route {self.route_id}>"
